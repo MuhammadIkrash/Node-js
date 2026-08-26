6d51f4c334f3e36c370bf63f03a6035745a5b5ca
Muhammad Ikrash <mikrash62@gmail.com>
2026-08-26 19:40:53 +0500

    Refactor ConnectDataBase function to use async/await for mongoose connection

--- express/Api_Development/Server/Config/DataBase.config.js
+++ express/Api_Development/Server/Config/DataBase.config.js
@@ -1,8 +1,8 @@
 import env from "./env.config.js";
 import mongoose from "mongoose";
-const ConnectDataBase = () => {
+const ConnectDataBase = async () => {
     try {
-        mongoose.connect(env.mongo_uri)
+        await mongoose.connect(env.mongo_uri)
             .then(() => console.log("Mongodb Connected SuccessFul"))
             .catch(err => console.log(err))
     } catch (error) {
